b65b5c038cb076876f7d573628c7d6c422d7d61c
praveen2407sonu <psnethrana@gmail.com>
2025-12-30 12:26:17 +0530

    updated

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 import { ClerkProvider } from "@clerk/clerk-react";
 import { DataProvider } from './context/DataContext.jsx';
 
@@ -10,9 +11,12 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <DataProvider>
+     <CartProvider>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <App/>
     </ClerkProvider>
+
+         </CartProvider>
     </DataProvider>
   </StrictMode>,
 )
